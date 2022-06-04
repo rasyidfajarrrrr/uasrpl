7e2dfbc3b24e327f3dd0e59e054d7e01b1a35301
Christian Wichmann <wichmann@bbs-os-brinkstr.de>
2022-06-04 11:58:53 +0200

    Create simple version of group monitor.

---
+++ README.md
@@ -0,0 +1,19 @@
+# group_monitor
+Simple web app to manage groups and show participants on monitors.
+
+## Usage
+Install all requirements before first use:
+
+    pip install flask faker
+
+To start the web app with the internal web server of Flask:
+
+    FLASK_ENV=development flask run
+
+Or run the Python script directly:
+
+    python gm.py
+
+## Requirements
+* flask
+* faker
---
+++ gm.py
@@ -0,0 +1,112 @@
+
+"""
+group_monitor - Simple web app to manage groups and show participants on monitors.
+
+"""
+
+from flask import Flask, redirect, render_template, abort, request, url_for
+
+from faker import Faker
+
+
+# create Faker instance for name generation and set locale
+fake = Faker(locale='de_DE')
+
+# initialize HTTP server
+app = Flask(__name__)
+
+
+# global list containing all participants for all groups
+GROUP_COUNT = 6
+groups = [('', '', '') for i in range(GROUP_COUNT)]
+
+
+@app.route('/', methods=['GET'])
+def start():
+    global groups
+    fake_names = True if 'fake' in request.args else False
+    notification = ''
+    if 'success' in request.args:
+        notification = 'Einträge wurden gespeichert!'
+    if 'cleared' in request.args:
+        notification = 'Einträge wurden gelöscht!'
+    return render_template('index.html', heading='Gruppen-Monitor', content=build_group_form(groups, fake_names=fake_names), notification=notification)
+
+
+def build_group_form(groups, fake_names=False):
+    content = '<h1 class="title"><a href="/">Gruppen-Monitor</a></h1><form action="/groups" method="POST">'
+    for i, g in enumerate(groups):
+        content += f"""<div class="group-block">
+            <h2 class="subtitle"><a href="{url_for('monitor', no=i)}" class="button is-primary">Gruppe {i+1}</a></h2>
+            <div class="field-group">
+            <div class="field is-inline-block-desktop ml-6">
+            <label class="label" for="group{i}-name1">Name 1</label>
+            <div class="control"><input class="input is-primary is-one-quarter" type="text" name="group{i}-name1" id="group{i}-name1" value="{fake.name() if fake_names else g[0]}"/></div>
+            </div>
+            <div class="field is-inline-block-desktop ml-6">
+            <label class="label" for="group{i}-name2">Name 2</label>
+            <div class="control"><input class="input is-primary is-one-quarter" type="text" name="group{i}-name2" id="group{i}-name2" value="{fake.name() if fake_names else g[1]}"/></div>
+            </div>
+            <div class="field is-inline-block-desktop ml-6">
+            <label class="label" for="group{i}-name3">Name 3</label>
+            <div class="control"><input class="input is-primary is-one-quarter" type="text" name="group{i}-name3" id="group{i}-name3" value="{fake.name() if fake_names else g[2]}"/></div>
+            </div>
+        </div>
+        """
+    content += '<div class="buttons is-right"><input class="button is-primary" type="submit" value="Speichern..."></div></form>'
+    return content
+
+
+@app.route('/monitor/<int:no>')
+def monitor(no):
+    global groups
+    try:
+        content = f"""<section class="hero is-primary is-large mt-6">
+            <div class="hero-body">
+                <p class="title is-size-1">
+                    Gruppenmitglieder Gruppe {no+1}
+                </p>
+                <p class="subtitle">
+                    {''.join(['<div class="box nametag">{}</div>'.format(n) for n in groups[no] if n])}
+                </p>
+            </div>
+            </section>
+        """
+        return render_template('index.html', heading=f'Gruppe Nr. {no+1}', content=content, notification='')
+    except:
+        return render_template('index.html', heading=f'Fehlerhafte Gruppennummer', content=f'Gruppe {no} gibt es nicht!', notification='')
+
+
+@app.route('/groups', methods=['GET', 'POST'])
+def add():
+    global groups
+    if request.method == 'GET':
+        group_block_html = """<div class="card"><div class="card-content">
+            <div class="media"><div class="media-content">
+                <p class="title is-4">{}</p>
+            </div></div>
+                <div class="content">{}</div>
+            </div></div>"""
+        content = '<h1 class="title">Gruppen</h1>' + \
+            '\n'.join([group_block_html.format(i+1, ', '.join([n for n in g]))
+                      for i, g in enumerate(groups)])
+        return render_template('index.html', notification='', content=content)
+    elif request.method == 'POST':
+        groups.clear()
+        for i in range(GROUP_COUNT):
+            groups.append((request.form.get(f'group{i}-name1'), request.form.get(
+                f'group{i}-name2'), request.form.get(f'group{i}-name3')))
+        return redirect(url_for('start', success=True))
+    else:
+        return abort(404)
+
+
+@app.route('/groups/clear')
+def clear():
+    global groups
+    groups = [('', '', '') for i in range(GROUP_COUNT)]
+    return redirect(url_for('start', cleared=True))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
